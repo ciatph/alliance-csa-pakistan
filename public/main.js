82837906c233e4966dae5e2c4d17b0061e791950
ciatph <ciat.ph@gmail.com>
2021-04-28 14:16:37 +0800

    Use relative data path.

--- public/main.js
+++ public/main.js
@@ -56,13 +56,13 @@ function click_tab (tab_selected) {
     $('#title').html('Cropping system')
     $('#description').html('In this tab you will find cropping and livestock data for each of the districts included in the study, based on the most recent provincial crop reporting data. Then for each of the villages in the province for which a CSV plan was prepared, you will find the cropping and hazard calendar constructed through village level consultations.')
     $('#cropping').removeClass('d-none')
-    d3.csv('https://raw.githubusercontent.com/CIAT-DAPA/pakistan_web/master/data/cropping/production.csv', function (error, data) {
+    d3.csv('/data/cropping/production.csv', function (error, data) {
       if (error) { console.log(error) }
       const c_production = data.filter(function (item) { return item.district === district })
-      d3.csv('https://raw.githubusercontent.com/CIAT-DAPA/pakistan_web/master/data/cropping/cropping_calendar.csv', function (error, data2) {
+      d3.csv('/data/cropping/cropping_calendar.csv', function (error, data2) {
         if (error) { console.log(error) }
         crop_c_full = data2.filter(function (item) { return item.district === district })
-        d3.csv('https://raw.githubusercontent.com/CIAT-DAPA/pakistan_web/master/data/cropping/hazard.csv', function (error, data3) {
+        d3.csv('/data/cropping/hazard.csv', function (error, data3) {
           if (error) { console.log(error) }
           hazard_c_full = data3.filter(function (item) { return item.district === district })
           cropping_fill(c_production)
@@ -97,7 +97,7 @@ function click_tab (tab_selected) {
     $('#title').html('Climate impacts')
     $('#description').html('This tab presents the results of the climate impact analysis where value chain experts in each of the districts identified the impacts of certain hazards on key commodity value chains for the district. The impacts are ordered with those that were selected the most frequently at the top. It is possible to further filter the impacts looking at specific hazards of commodity value chains.')
     $('#impacts').removeClass('d-none')
-    d3.csv('https://raw.githubusercontent.com/CIAT-DAPA/pakistan_web/master/data/impacts/impacts.csv', function (error, data) {
+    d3.csv('/data/impacts/impacts.csv', function (error, data) {
       if (error) { console.log(error) }
       hazard_d = data.filter(function (item) { return item.district === district })
       // Filling the cbo controls with hazard and crops
@@ -134,7 +134,7 @@ function click_tab (tab_selected) {
     $('#title').html('Climate risk')
     $('#description').html('This tab presents a risk matrix generated through responses from agricultural experts in the districts on the frequency and severity of major hazards.')
     $('#risk').removeClass('d-none')
-    d3.csv('https://raw.githubusercontent.com/CIAT-DAPA/pakistan_web/master/data/risk/risk.csv', function (error, data) {
+    d3.csv('/data/risk/risk.csv', function (error, data) {
       if (error) { console.log(error) }
       const risk_d = data.filter(function (item) { return item.district === district })
       risk_fill(risk_d)
@@ -143,7 +143,7 @@ function click_tab (tab_selected) {
     $('#title').html('Practices')
     $('#description').html('This tab presents the types of CSA practices prioritised by experts in the district, along with the areas were they have an impact, the hazards they address, and the barriers to adoption. the practices can be filtered by the commodity they look at, the areas where they have an impact or the types of hazards they address, allowing decision makers to identify practices that address certain priority issues.')
     $('#practices').removeClass('d-none')
-    d3.csv('https://raw.githubusercontent.com/CIAT-DAPA/pakistan_web/master/data/practices/practices.csv', function (error, data) {
+    d3.csv('/data/practices/practices.csv', function (error, data) {
       if (error) { console.log(error) }
       practices_d = data.filter(function (item) { return item.district === district })
 
@@ -197,7 +197,7 @@ function click_tab (tab_selected) {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map)
 
-  const layer_url = 'https://raw.githubusercontent.com/CIAT-DAPA/pakistan_web/master/data/maps/pakistan_map.json'
+  const layer_url = '/data/maps/pakistan_map.json'
   $.getJSON(layer_url, function (data) {
     L.geoJSON(data, { onEachFeature: onEachFeature }).addTo(map)
   })
@@ -304,16 +304,16 @@ function cropping_calendar_hazard (crop_c, hazard_c) {
  * Function which loads and draws all plots for climate section
  */
 function climate_load () {
-  d3.csv('https://raw.githubusercontent.com/CIAT-DAPA/pakistan_web/master/data/climate/climatology.csv', function (error, data) {
+  d3.csv('/data/climate/climatology.csv', function (error, data) {
     if (error) { console.log(error) }
     const climatology = data.filter(function (item) { return item.district === district })
-    d3.csv('https://raw.githubusercontent.com/CIAT-DAPA/pakistan_web/master/data/climate/gcm.csv', function (error, data2) {
+    d3.csv('/data/climate/gcm.csv', function (error, data2) {
       if (error) { console.log(error) }
       const gcm = data2.filter(function (item) { return item.district === district })
 
       climate_fill(climatology, gcm)
 
-      d3.csv('https://raw.githubusercontent.com/CIAT-DAPA/pakistan_web/master/data/climate/indicators.csv', function (error, data3) {
+      d3.csv('/data/climate/indicators.csv', function (error, data3) {
         if (error) { console.log(error) }
         climate_indicator_d = data3.filter(function (item) { return item.district === district })
 
@@ -605,7 +605,7 @@ function climate_indicator_fill (indicator, season) {
     .domain([min, max])
     .range([0, 1])
 
-  const indicator_url = 'https://raw.githubusercontent.com/CIAT-DAPA/pakistan_web/master/data/climate/' + season + '_new_version.json'
+  const indicator_url = '/data/climate/' + season + '_new_version.json'
   $.getJSON(indicator_url, function (data) {
     L.geoJSON(data, { filter: filter_map, onEachFeature: onEachFeature2, weight: 0.5 }).addTo(map_i)
   })
@@ -784,7 +784,7 @@ function risk_fill (data) {
 
 function enablers_load () {
   type_agg = $('#cbo_enablers_agg').val()
-  d3.csv('https://raw.githubusercontent.com/CIAT-DAPA/pakistan_web/master/data/enablers/enablers_' + type_agg + '.csv', function (error, data) {
+  d3.csv('/data/enablers/enablers_' + type_agg + '.csv', function (error, data) {
     if (error) { console.log(error) }
     let enablers = null
     if (type_agg === 'dist') { enablers = data.filter(function (item) { return item.district === district }) } else if (type_agg === 'prov') { enablers = data.filter(function (item) { return item.province === province }) } else { enablers = data }
@@ -815,7 +815,7 @@ function enablers_fill (data) {
  * Function for each feature into the map
  */
 function onEachFeature (feature, layer) {
-  d3.csv('https://raw.githubusercontent.com/CIAT-DAPA/pakistan_web/master/data/maps/pakistan_data.csv', function (error, data) {
+  d3.csv('/data/maps/pakistan_data.csv', function (error, data) {
     if (error) { console.log(error) }
     const district = data.filter(function (item) { return item.NAME_3 === layer.feature.properties.NAME_3 })
     layer.setStyle({
